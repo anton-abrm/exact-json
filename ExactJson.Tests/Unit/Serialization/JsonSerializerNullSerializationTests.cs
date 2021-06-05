fa6d0d9bb7a7b2e737bc37ecb1e35f47f129079f
Anton Abramenko <anton@abramenko.dev>
2021-06-05 22:05:46 +0600

    Add tests for SerializeNullProperty

---
+++ ExactJson.Tests/Unit/Serialization/JsonSerializerNullSerializationTests.cs
@@ -0,0 +1,51 @@
+﻿using ExactJson.Serialization;
+
+using NUnit.Framework;
+
+namespace ExactJson.Tests.Unit.Serialization
+{
+    public class JsonSerializerNullPropertySerializationTests
+    {
+        private sealed class DemoClass
+        {
+            [JsonNode("foo"), JsonOptional]
+            public string Foo { get; set; }
+        }
+        
+        [Test]
+        public void SerializeNullProperty_FalseByDefault()
+        {
+            var serializer = new JsonSerializer();
+            
+            Assert.That(serializer.SerializeNullProperty, Is.False);
+        }
+        
+        [Test]
+        public void SerializeNullProperty_False()
+        {
+            var demo = new DemoClass();
+            
+            var serializer = new JsonSerializer() {
+                SerializeNullProperty = false
+            };
+
+            var json = serializer.Serialize<DemoClass>(demo);
+            
+            Assert.That(json, Is.EqualTo("{}"));
+        }
+        
+        [Test]
+        public void SerializeNullProperty_True()
+        {
+            var demo = new DemoClass();
+            
+            var serializer = new JsonSerializer() {
+                SerializeNullProperty = true
+            };
+
+            var json = serializer.Serialize<DemoClass>(demo);
+            
+            Assert.That(json, Is.EqualTo("{\"foo\":null}"));
+        }
+    }
+}
