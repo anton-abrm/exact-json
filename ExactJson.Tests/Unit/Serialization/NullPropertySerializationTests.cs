15da2294d0fdfb15f033944277cef93d69322a15
Anton Abramenko <anton@abramenko.dev>
2021-06-06 21:56:25 +0600

    Add tests for JsonSerializeNull attribute

--- ExactJson.Tests/Unit/Serialization/NullPropertySerializationTests.cs
+++ ExactJson.Tests/Unit/Serialization/NullPropertySerializationTests.cs
@@ -6,8 +6,29 @@ namespace ExactJson.Tests.Unit.Serialization
 {
     public class NullPropertySerializationTests
     {
-        private sealed class DemoClass
+        private sealed class NoNullAttributeClass
         {
+            [JsonNode("foo"), JsonOptional]
+            public string Foo { get; set; }
+        }
+        
+        private sealed class DefaultNullAttributeClass
+        {
+            [JsonSerializeNull]
+            [JsonNode("foo"), JsonOptional]
+            public string Foo { get; set; }
+        }
+        
+        private sealed class TrueNullAttributeClass
+        {
+            [JsonSerializeNull(true)]
+            [JsonNode("foo"), JsonOptional]
+            public string Foo { get; set; }
+        }
+        
+        private sealed class FalseNullAttributeClass
+        {
+            [JsonSerializeNull(false)]
             [JsonNode("foo"), JsonOptional]
             public string Foo { get; set; }
         }
@@ -21,31 +42,67 @@ namespace ExactJson.Tests.Unit.Serialization
         }
         
         [Test]
-        public void SerializeNullProperty_False()
+        public void NoNullAttribute_SerializeNullProperty_False()
         {
-            var demo = new DemoClass();
+            var obj = new NoNullAttributeClass();
             
             var serializer = new JsonSerializer() {
                 SerializeNullProperty = false
             };
 
-            var json = serializer.Serialize<DemoClass>(demo);
+            var json = serializer.Serialize<NoNullAttributeClass>(obj);
             
             Assert.That(json, Is.EqualTo("{}"));
         }
         
         [Test]
-        public void SerializeNullProperty_True()
+        public void NoNullAttribute_SerializeNullProperty_True()
         {
-            var demo = new DemoClass();
+            var obj = new NoNullAttributeClass();
             
             var serializer = new JsonSerializer() {
                 SerializeNullProperty = true
             };
 
-            var json = serializer.Serialize<DemoClass>(demo);
+            var json = serializer.Serialize<NoNullAttributeClass>(obj);
             
             Assert.That(json, Is.EqualTo("{\"foo\":null}"));
+        }
+
+        [Test]
+        public void DefaultNullAttribute()
+        {
+            var obj = new DefaultNullAttributeClass();
+            
+            var serializer = new JsonSerializer();
+
+            var json = serializer.Serialize<DefaultNullAttributeClass>(obj);
+            
+            Assert.That(json, Is.EqualTo("{\"foo\":null}"));
+        }
+        
+        [Test]
+        public void TrueNullAttribute()
+        {
+            var demo = new TrueNullAttributeClass();
+            
+            var serializer = new JsonSerializer();
+
+            var json = serializer.Serialize<TrueNullAttributeClass>(demo);
+            
+            Assert.That(json, Is.EqualTo("{\"foo\":null}"));
+        }
+        
+        [Test]
+        public void FalseNullAttribute()
+        {
+            var obj = new FalseNullAttributeClass();
+            
+            var serializer = new JsonSerializer();
+
+            var json = serializer.Serialize<FalseNullAttributeClass>(obj);
+            
+            Assert.That(json, Is.EqualTo("{}"));
         }
     }
 }
