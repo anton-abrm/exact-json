9321112f5755d88d420f208b81ea3f6ce8e43723
Anton Abramenko <anton@abramenko.dev>
2021-06-18 23:18:50 +0600

    Add tests for JsonUriConverter

---
+++ ExactJson.Tests/Unit/Serialization/Converters/JsonUriConverterTests.cs
@@ -0,0 +1,59 @@
+using System;
+
+using ExactJson.Serialization;
+using ExactJson.Serialization.Converters;
+
+using NUnit.Framework;
+
+namespace ExactJson.Tests.Unit.Serialization.Converters
+{
+    public sealed class JsonUriConverterTests
+    {
+        [TestCase("https://example.com/")]
+        public void GetString(string version)
+        {
+            var converter = new JsonUriConverter();
+
+            var result = converter.GetString(new Uri(version), new JsonConverterContext());
+
+            Assert.That(result, Is.EqualTo(version));
+        }
+
+        [TestCase("https://example.com/")]
+        public void GetValue(string version)
+        {
+            var converter = new JsonUriConverter();
+
+            var result = converter.GetValue(version, new JsonConverterContext());
+
+            Assert.That(result, Is.EqualTo(new Uri(version)));
+        }
+
+        [Test]
+        public void GetString_NullValue_ThrowsArgumentNullException()
+        {
+            var converter = new JsonUriConverter();
+
+            Assert.Throws<ArgumentNullException>(() 
+                => converter.GetString(null, new JsonConverterContext()));
+        }
+        
+        [Test]
+        public void GetValue_NullString_ThrowsArgumentNullException()
+        {
+            var converter = new JsonUriConverter();
+
+            Assert.Throws<ArgumentNullException>(() 
+                => converter.GetValue(null, new JsonConverterContext()));
+        }
+        
+        [Test]
+        public void GetValue_InvalidString_ThrowsJsonInvalidValueException()
+        {
+            var converter = new JsonUriConverter();
+
+            Assert.Throws<JsonInvalidValueException>(() 
+                => converter.GetValue("", new JsonConverterContext()));
+        }
+    }
+}
